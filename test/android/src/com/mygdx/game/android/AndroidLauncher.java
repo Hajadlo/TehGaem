7859658892b60c08e37c0a32643be62c10561878
Hajadlo <Hajadlo@gmail.com>
2015-05-27 13:17:25 +0200

    XML BIČÍŠ!

--- test/android/src/com/mygdx/game/android/AndroidLauncher.java
+++ test/android/src/com/mygdx/game/android/AndroidLauncher.java
@@ -1,16 +1,15 @@
 package com.mygdx.game.android;
 
 import android.os.Bundle;
-
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.MojeCoolHra;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new MyGdxGame(), config);
+		initialize(new MojeCoolHra(), config);
 	}
 }
